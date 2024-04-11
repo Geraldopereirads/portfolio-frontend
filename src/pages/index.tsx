e8b1181485e09bd8e93a81f011494139502c36a6
geraldopereirads <geraldo.pereirads@hotmail.com>
2024-04-11 11:25:49 -0300

    feat: finish homePage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,34 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
 import Header from "@/components/header/header";
+import Image from "next/image";
+import fullstackDeveloper from "../assets/img/home/dev.svg";
+import { NextPage } from "next";
 
-const inter = Inter({ subsets: ["latin"] });
-
-export default function Home() {
+const Home: NextPage = () => {
   return (
-    <main>
-      <Header/>
-    </main>
+    <>
+      <Header />
+      <main >
+        <div >
+          <h1 >
+            <small>Olá</small> <br />
+            Eu sou Geraldo Pereira, Desenvolvedor Full-Stack
+          </h1>
+          <h2>Seja bem-vindo ao meu portólio website</h2>
+          <a
+            href=""
+          >
+            Saiba mais sobre mim
+          </a>
+        </div>
+
+        <Image
+          src={fullstackDeveloper}
+          alt="Desenvolvedor Full-Stack"
+          width={300}
+          height={200}
+        />
+      </main>
+    </>
   );
-}
+};
+export default Home;
