710fd95fb52f6f65161bdd040b7a7f8020fd6890
geraldopereirads <geraldo.pereirads@hotmail.com>
2024-04-12 08:39:34 -0300

    feat: add portfolio page

---
+++ src/pages/portfolio.tsx
@@ -0,0 +1,15 @@
+import Header from "@/components/header/header";
+import { NextPage } from "next";
+
+const Portfolio: NextPage = () => {
+  return (
+    <>
+      <Header />
+      <main>
+        <h1>Portfólio</h1>
+      </main>
+    </>
+  );
+};
+
+export default Portfolio;
