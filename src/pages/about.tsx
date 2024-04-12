f2c82e5ac5151ba327ade9802379fe20c6b63d45
geraldopereirads <geraldo.pereirads@hotmail.com>
2024-04-12 08:08:13 -0300

    feat: add about page

---
+++ src/pages/about.tsx
@@ -0,0 +1,83 @@
+import Header from "@/components/header/header";
+import { NextPage } from "next";
+
+const About: NextPage = () => {
+  return (
+    <>
+      <Header />
+      <main>
+        <h1>
+          <span>
+            Geraldo
+          </span>
+          <span>
+            Pereira
+          </span>
+        </h1>
+        <div>
+          <p>
+            Olá! Sou desenvolvedor Full-stack com mais de um ano de experiência
+            em desenvolvimento web. Durante minha jornada, concluí diversos
+            projetos significativos que demonstram minhas habilidades e
+            expertise na área.
+          </p>
+          <p>
+            Um desses projetos é uma aplicação para uma hamburgueria fictícia,
+            construída utilizando React. Essa aplicação se destaca por sua
+            integração com uma API para obtenção de produtos, funcionalidades
+            avançadas como adicionar produtos ao carrinho e exibição do valor
+            total da compra. Além disso, a estilização é responsiva, seguindo as
+            melhores práticas de mobile first.
+          </p>
+          <p>
+            Outro projeto relevante é uma aplicação de listagem de tecnologias
+            desenvolvida em React e TypeScript. Essa aplicação inclui recursos
+            como cadastro e login de usuários, além da capacidade de cadastrar e
+            remover tecnologias. Para garantir a qualidade e segurança, utilizei
+            tecnologias como React Hook Form, Yup, React-Toastify e Context API,
+            juntamente com Styled Components para a estilização responsiva.
+          </p>
+        </div>
+        <ul>
+          <li>Organização</li>
+          <li>Criativo</li>
+          <li>Comunicação</li>
+          <li>Eficiência</li>
+        </ul>
+        <picture>
+          <img
+            src="https://github.com/Geraldopereirads.png"
+            alt="Desenvolvedor Full-Stack"
+            width={300}
+            height={300}
+          />
+        </picture>
+
+        <ul>
+          <li>Adaptabilidade</li>
+          <li>Curioso</li>
+          <li>Resolução</li>
+          <li>Lógica</li>
+        </ul>
+        <div>
+          <p>
+            Além disso, desenvolvi uma API para uma locadora de filmes
+            fictícios, utilizando tecnologias de ponta, como TypeScript, Zod,
+            PostgreSQL, TypeORM, PG e PG-Format. Essa API permite realizar
+            operações de CRUD completo de filmes, demonstrando minha competência
+            na construção de sistemas robustos e escaláveis.
+          </p>
+          <p>
+            Estou sempre em busca de novos desafios e estou animado para
+            continuar contribuindo com meus conhecimentos e habilidades em
+            projetos futuros. Se você busca um profissional com paixão pelo
+            desenvolvimento, estou à disposição para colaborar em sua próxima
+            empreitada.
+          </p>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default About;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,30 @@
-import Header from "@/components/header/header";
-import Image from "next/image";
-import fullstackDeveloper from "../assets/img/home/dev.svg";
-import { NextPage } from "next";
+import Header from '@/components/header/header'
+import Image from 'next/image'
+import fullstackDeveloper from '../assets/img/home/dev.svg'
+import { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
     <>
       <Header />
-      <main >
-        <div >
-          <h1 >
+      <main>
+        <div>
+          <h1>
             <small>Olá</small> <br />
             Eu sou Geraldo Pereira, Desenvolvedor Full-Stack
           </h1>
           <h2>Seja bem-vindo ao meu portólio website</h2>
-          <a
-            href=""
-          >
-            Saiba mais sobre mim
-          </a>
+          <a href=''>Saiba mais sobre mim</a>
         </div>
 
         <Image
           src={fullstackDeveloper}
-          alt="Desenvolvedor Full-Stack"
+          alt='Desenvolvedor Full-Stack'
           width={300}
           height={200}
         />
       </main>
     </>
-  );
-};
-export default Home;
+  )
+}
+export default Home
