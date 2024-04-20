1f704fa25df77139a571c38415b0e4a723359962
geraldopereirads <geraldo.pereirads@hotmail.com>
2024-04-20 16:23:51 -0300

    feat: add tools page

---
+++ src/pages/tools.tsx
@@ -0,0 +1,23 @@
+import Header from "@/components/header/header";
+import { NextPage } from "next";
+
+const Tools: NextPage = () => {
+  return (
+    <>
+      <Header />
+      <main>
+        <h1>Ferramentas</h1>
+
+        <div>
+          <h2>Tecnologias</h2>
+          <small>Atualmente Desenvolvendo</small>
+        </div>
+
+        <ul>
+            {}
+        </ul>
+      </main>
+    </>
+  );
+};
+export default Tools;
