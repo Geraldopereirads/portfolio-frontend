35b6b0ac23ddea1e7fc9f0ee9f89dca014c59bfd
geraldopereirads <geraldo.pereirads@hotmail.com>
2024-04-20 22:24:13 -0300

    feat: add interface tools

--- src/interface/tools.interface.ts
+++ src/interface/tools.interface.ts
@@ -2,3 +2,7 @@ import { z } from "zod";
 import { ToolsSchema } from "./../schemas/tools.schema";
 
 export type ToolsData = z.infer<typeof ToolsSchema>;
+
+export interface ToolsProps {
+    title:ToolsData[]
+}
