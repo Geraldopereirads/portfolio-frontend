264e03c3c7630d269847c808f59c4e228379ccdf
geraldopereirads <geraldo.pereirads@hotmail.com>
2024-04-25 18:11:12 -0300

    feat: addd filter for title in backend page

--- src/pages/[portfolio]/backend.tsx
+++ src/pages/[portfolio]/backend.tsx
@@ -1,16 +1,24 @@
 import Header from "@/components/header/header";
 import { CardsProps } from "@/interface/projects.interface";
 import { api } from "@/services/api";
-import {
-  GetServerSideProps,
-  GetStaticPaths,
-  GetStaticProps,
-  NextPage,
-} from "next";
+import { GetStaticPaths, GetStaticProps, NextPage } from "next";
 import Link from "next/link";
 import Card from "../../components/portfolioPage/cards/backEnd.card";
+import { useState } from "react";
 
 const BackEnd: NextPage<CardsProps> = ({ cards }: CardsProps) => {
+  const [search, setSearch] = useState("");
+
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value); // Atualize o estado `search` com o valor digitado pelo usuário
+  };
+
+  const searchLowerCase = search.toLowerCase();
+
+  const filterProjects = cards.filter((project) =>
+    project.title.toLowerCase().includes(searchLowerCase)
+  );
+
   return (
     <>
       <Header />
@@ -25,8 +33,18 @@ const BackEnd: NextPage<CardsProps> = ({ cards }: CardsProps) => {
           <Link href="/portfolio/fullstack">Full-Stack</Link>
         </nav>
 
+        <form>
+          <input
+            className="w-96 text-black"
+            type="search"
+            value={search}
+            onChange={handleSearchChange}
+            placeholder="Insira o título do projeto que você está buscando"
+          />
+        </form>
+
         <ul>
-          {cards.map((project) => (
+          {filterProjects.map((project) => (
             <Card key={project.id} card={project} />
           ))}
         </ul>
