52707ef97ecc4cd6125cc76289deecd51db28292
geraldopereirads <geraldo.pereirads@hotmail.com>
2024-05-01 00:03:59 -0300

    feat: finish contact page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,30 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { register } from "swiper/element/bundle";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />;
+      <ToastContainer
+        position="top-right"
+        autoClose={1000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+      <ToastContainer />
+    </>
+  );
 }
--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,7 +1,48 @@
 import Header from "@/components/header/header";
 import { NextPage } from "next";
+import { useState } from "react";
+import { toast } from "react-toastify";
+import emailjs from "@emailjs/browser";
 
 const Contact: NextPage = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [whatsapp, setWhatsapp] = useState("");
+  const [message, setMessage] = useState("");
+
+  function sendEmail(e: any) {
+    e.preventDefault();
+    if (name === "" || email === "" || message === "") {
+      toast.error("Preencha todos os campos");
+    } else {
+      const templateParams = {
+        from_name: name,
+        message: message,
+        whatsapp: whatsapp,
+        email: email,
+      };
+      emailjs
+        .send(
+          "service_lgjwc3o",
+          "template_l9cgi6r",
+          templateParams,
+          "bL3i2klCcpK676Raf"
+        )
+        .then(
+          (res) => {
+            toast.success("Menssagem enviada com sucesso");
+            setName("");
+            setWhatsapp("");
+            setEmail("");
+            setMessage("");
+          },
+          (err) => {
+            toast.error("Erro ao enviar email");
+          }
+        );
+    }
+  }
+
   return (
     <>
       <Header />
@@ -11,12 +52,31 @@ const Contact: NextPage = () => {
           Desperte sua próxima ideia incrivel e transforme projetos em
           realidade!
         </p>
-        <form>
-          <input type="text" placeholder="Digite seu nome" />
-          <input type="text" placeholder="Digite seu WhatsApp" />
-          <input type="text" placeholder="Digite seu E-mail" />
-          <textarea placeholder="Digite sua mensagem..."></textarea>
-          <input type="submit" value="Enviar" />
+        <form onSubmit={sendEmail}>
+          <input
+            type="text"
+            placeholder="Digite seu nome"
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          />
+          <input
+            type="text"
+            placeholder="Digite seu WhatsApp"
+            onChange={(e) => setWhatsapp(e.target.value)}
+            value={whatsapp}
+          />
+          <input
+            type="text"
+            placeholder="Digite seu E-mail"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
+          <textarea
+            placeholder="Digite sua mensagem..."
+            onChange={(e) => setMessage(e.target.value)}
+            value={message}
+          ></textarea>
+          <input type="submit" value="Enviar" className="cursor-pointer"/>
         </form>
       </main>
     </>
