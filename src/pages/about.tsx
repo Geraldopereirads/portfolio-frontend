ed68f267a363241fabf77a035d66d7dbf46de801
geraldopereirads <geraldo.pereirads@hotmail.com>
2024-06-05 17:56:43 -0300

    style: add style about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -7,9 +7,9 @@ const About: NextPage = () => {
       <Header />
       <main className="flexCol">
         <h1 className="primaryTitle">Geraldo Pereira</h1>
-        <section>
-          <p>
-            Desenvolvedor Full Stack Jr. com sólida formação em Desenvolvimento
+        <section className="mt-20 mb-20">
+          <p className="paragraph leading-[220%]">
+            Desenvolvedor Full Stack. com sólida formação em Desenvolvimento
             Web Full Stack pela Kenzie Academy Brasil e graduação em andamento
             em Engenharia de Software. Possuo conhecimento abrangente em
             desenvolvimento Front End e Back End, com foco na criação de
@@ -19,12 +19,6 @@ const About: NextPage = () => {
             Inglês intermediário.
           </p>
         </section>
-        <ul className="flex flex-row">
-          <li>Organização</li>
-          <li>Criativo</li>
-          <li>Comunicação</li>
-          <li>Eficiência</li>
-        </ul>
         <picture>
           <img
             src="https://github.com/Geraldopereirads.png"
@@ -34,15 +28,8 @@ const About: NextPage = () => {
             className="rounded-full"
           />
         </picture>
-
-        <ul className="flex flex-row">
-          <li>Adaptabilidade</li>
-          <li>Curioso</li>
-          <li>Resolução</li>
-          <li>Lógica</li>
-        </ul>
-        <section>
-          <p>
+        <section className="mt-20">
+          <p className="paragraph leading-[220%]">
             Experiência sólida na construção de projetos utilizando stacks como
             HTML, CSS, JavaScript ES6, React, TypeScript, Node.js, Python e
             Next.js. Desenvolvo aplicações por meio de metodologias ágeis
