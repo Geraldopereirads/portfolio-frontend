aeec9af13f2e4848b3c6fad2d34ea5f8fdfb53b6
geraldopereirads <geraldo.pereirads@hotmail.com>
2024-06-07 21:39:35 -0300

    style: add style fullstack page

--- src/components/portfolioPage/cards/fullStack.card.tsx
+++ src/components/portfolioPage/cards/fullStack.card.tsx
@@ -5,10 +5,15 @@ import { FaGithub } from "react-icons/fa";
 
 const Card = ({ card }: CardProps) => {
   return (
-    <li>
-      <Link href={card.url} target="_blank">
-        Em Construção
+    <li className=" flex flex-col justify-center items-center gap-2">
+      <Link className="text text-2xl" href={card.url} target="_blank">
+        Portfólio Full-Stack
       </Link>
+      <div className="flex justify-center items-center gap-5">
+        <Link href={card.github} target="_blank">
+          <FaGithub size={25} />
+        </Link>
+      </div>
       <Link href={card.url} target="_blank">
         <Image
           width={300}
@@ -17,11 +22,6 @@ const Card = ({ card }: CardProps) => {
           alt="Capa dos projetos FullStack"
         />
       </Link>
-      <div>
-        <Link href={card.github} target="_blank">
-          <FaGithub size={25} />
-        </Link>
-      </div>
     </li>
   );
 };
