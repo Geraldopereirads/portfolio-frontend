d29dcbe54068b08ad2ed191889d134256c7b2d9b
geraldopereirads <geraldo.pereirads@hotmail.com>
2024-06-11 14:37:54 -0300

    feat: trade url api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://portfolio-frontend-5bub.onrender.com",
+  baseURL: "https://portfolio-backend-c9x0.onrender.com",
   timeout: 5000,
 });
